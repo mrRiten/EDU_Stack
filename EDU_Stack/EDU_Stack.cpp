b6486347428dabaf0c8f7eb2f2f71ca2274565cb
mrRiten <vitalik.chu@mail.ru>
2024-02-03 19:36:56 +0300

    Добавьте файлы проекта.

---
+++ EDU_Stack/EDU_Stack.cpp
@@ -0,0 +1,63 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+class Stack {
+public:
+	Stack(int data) {
+		this->data = data;
+		this->nextItem = nullptr;
+	}
+	Stack(int data, Stack* nextItem) : Stack(data) {
+		this->nextItem = nextItem;
+	}
+
+	int getData() const {
+		return this->data;
+	}
+
+	Stack* getNextItem() const {
+		return this->nextItem;
+	}
+
+private:
+	int data;
+	Stack* nextItem;
+};
+
+
+class StackList {
+public:
+	StackList(Stack* firstItem) {
+		this->firstItem = firstItem;
+	}
+
+	void addItem(int data) {
+		this->firstItem = new Stack(data, this->firstItem);
+	}
+
+	void delLastItem() {
+		Stack* delItem = firstItem;
+		firstItem = firstItem->getNextItem();
+		delete delItem;
+	}
+
+	void printItems() {
+		Stack* currentItem = firstItem;
+		cout << "Data for StackList:\n";
+		while (currentItem != nullptr)
+		{
+			printf("Stack data: %d\n", currentItem->getData());
+			currentItem = currentItem->getNextItem();
+		}
+	}
+
+private:
+	Stack* firstItem;
+};
+
+void main() {
+	StackList stackList = StackList(new Stack(10));
+
+}
